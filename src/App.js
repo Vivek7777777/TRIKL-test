b2526f928a0b15d927a456b81d70ea845d097e2d
Vivek <vivek.rbj.up@gmail.com>
2023-09-03 19:21:13 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import Draggable from 'react-draggable';
 import './App.css';
 
+let apikey = "MuEhlmtd-8jmoylP1xlwF3dhqWysRM-oLncKlSxr6dM";
+
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  const [backImg, setbackImg] = useState("");
+
+  useEffect( () => {
+    // fetch('https://api.unsplash.com/photos/random?client_id=' + apikey)
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+        // setbackImg(data.urls.regular);
+        setbackImg(data.message);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Draggable> */}
+        {/* <div className='text' contenteditable="true">{posts.alt_description}</div> */}
+      {/* </Draggable> */}
+      <div className='bgimg'>
+        <img className='backimg' src={backImg} alt='image' />
+      </div>
+    </>
   );
 }
 
